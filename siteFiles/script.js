73ecc0ab41b04550b8a41377c76a34a4bf0a9022
Alexander Effanga <alexandereffanga@Alexanders-Air.attlocal.net>
2022-07-09 20:48:19 -0500

    for clone

--- siteFiles/script.js
+++ siteFiles/script.js
@@ -6,7 +6,8 @@ function adviceRandom(min, max){
     return Math.floor(Math.random() * (max-min)) -min;
 }
 
-window.onload = () => {
+window.onload = e => {
+    e.preventDefault();
     makeAdvice();
 }
 
@@ -16,7 +17,7 @@ function makeAdvice() {
         if(xhr.status == 200){
             console.log("success!");
             adviceText.innerHTML = JSON.parse(this.response).slip.advice;
-            adviceId.innerHTML = JSON.parse(this.response).slip.id;
+            adviceId.innerHTML = "Advice Number #" + JSON.parse(this.response).slip.id;
         }
     }
     xhr.open('GET', 'https://api.adviceslip.com/advice/'+adviceRandom(1, 224) , true);
